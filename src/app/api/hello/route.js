dc1f0f812d8c2185912547a09384d6d65ef2d3e1
AlejandroFFXV <ericalejandro3001@gmail.com>
2024-04-01 13:07:39 -0600

    error

--- src/app/api/hello/route.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { conn } from "@/libs/mysql";
-
-export async function GET() {
-  // consulta de la fecha con JS y despliegue
-  const result = await conn.query("SELECT NOW()");
-  console.log(result);
-  return NextResponse.json({ message: result[0]["NOW()"] });
-}
