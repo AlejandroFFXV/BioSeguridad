26035f906cacb4aad70eed43f08f84523da45d02
AlejandroFFXV <ericalejandro3001@gmail.com>
2024-04-29 10:36:54 -0600

    obtener_datos MOD PORT

--- src/app/api/test/data/obtener_datos.py
+++ src/app/api/test/data/obtener_datos.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 import cv2
 import numpy as np
 import mediapipe as mp
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -62,4 +63,7 @@ def obtener_datos():
 
 
 if __name__ == "__main__":
-    app.run()
+    port = int(
+        os.environ.get("PORT", 5000)
+    )  # Use el puerto 5000 si no se proporciona ninguno
+    app.run(host="0.0.0.0", port=port)
