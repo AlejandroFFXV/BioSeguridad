6c908de305732677d5fa12bf4ed84ccf19fd6fff
AlejandroFFXV <ericalejandro3001@gmail.com>
2024-04-29 13:29:03 -0600

    commit

--- src/app/api/faces/route.js
+++ src/app/api/faces/route.js
@@ -35,7 +35,7 @@ export async function POST(request) {
     const filePath = path.join(process.cwd(), "public", image.name);
     await writeFile(filePath, buffer);
     const flaskResponse = await axios.post(
-      "https://obscure-taiga-29510-b894311fe9ca.herokuapp.com/obtener_datos",
+      "https://bioseguridad-5a2982caf0ec.herokuapp.com/obtener_datos",
       formData,
       {
         headers: {
