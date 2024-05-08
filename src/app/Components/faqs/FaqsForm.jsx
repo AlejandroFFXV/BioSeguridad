36158623c05bafe3fe0fb46a46573b40126312a4
AlejandroFFXV <ericalejandro3001@gmail.com>
2024-05-07 21:50:30 -0600

    commit final antes exposicion - olvide modificar faqs

--- src/app/Components/faqs/FaqsForm.jsx
+++ src/app/Components/faqs/FaqsForm.jsx
@@ -52,7 +52,7 @@ const FaqsForm = () => {
       <div className="flex items-center justify-center h-screen">
         <div className="max-w-md mx-auto relative overflow-hidden z-10 bg-gray-800 p-8 rounded-lg shadow-md before:w-24 before:h-24 before:absolute before:bg-purple-600 before:rounded-full before:-z-10 before:blur-2xl after:w-32 after:h-32 after:absolute after:bg-sky-400 after:rounded-full after:-z-10 after:blur-xl after:top-24 after:-right-12">
           <h2 className="text-2xl font-bold text-white mb-6">
-            Update Your Profile
+            Envia tus dudas!
           </h2>
 
           <form onSubmit={handleSubmit} ref={form}>
@@ -92,7 +92,7 @@ const FaqsForm = () => {
 
             <div className="flex justify-end">
               <button className="bg-gradient-to-r from-purple-600 via-purple-400 to-blue-500 text-white px-4 py-2 font-bold rounded-md hover:opacity-80">
-                Update Profile
+                Enviar
               </button>
             </div>
           </form>
