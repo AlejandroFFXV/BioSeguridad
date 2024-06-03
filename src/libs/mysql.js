523aaec1baea02c2398587516134ebe1e8086a0a
Eric Alejandro Magdaleno Martinez <90100623+AlejandroFFXV@users.noreply.github.com>
2024-06-03 11:28:45 -0600

    Update mysql.js

--- src/libs/mysql.js
+++ src/libs/mysql.js
@@ -2,20 +2,10 @@ import mysql from "serverless-mysql";
 // Como conectar una base de datos con NextJS en src creamos la carpeta libs y en dicha carpeta nuestro archivo
 export const conn = mysql({
   config: {
-    host: "mna97msstjnkkp7h.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: "yrnmapz66r0wdm88",
-    password: "ny7f0qnu63fcc4oz",
-    port: "3306",
-    database: "hjqazw0ynzxk0qbf",
+    host: "Tus credenciales",
+    user: "Tus credenciales",
+    password: "Tus credenciales",
+    port: "Tus credenciales",
+    database: "Tus credenciales",
   },
 });
-// Como conectar una base de datos con NextJS en src creamos la carpeta libs y en dicha carpeta nuestro archivo
-// export const conn = mysql({
-//   config: {
-//     host: "localhost",
-//     user: "root",
-//     password: "NeroDiosa/Wa1fu",
-//     port: "3306",
-//     database: "bioseguridad",
-//   },
-// });
